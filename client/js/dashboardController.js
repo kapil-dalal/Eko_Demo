2eedadbd5e868c61a155bd98f58f7987194f553a
rupesh1085 <marsaaap1>
2016-02-12 18:04:57 +0530

    ok

--- client/js/dashboardController.js
+++ client/js/dashboardController.js
@@ -1,15 +1,16 @@
 
         //Controller
 (function (angular) {
-    app.controller('dashboardController', ['$scope', '$state', 'Contact_us', function (scope, state, ContactUs) {
-        console.log("dashboardController controller");
-
+    app.controller('dashboardController', ['$scope', '$state', 'Contact_us','$anchorScroll', '$location', function (scope, state, ContactUs, anchorScroll, location) {
+        scope.scrollTo = function(id) {
+            location.hash(id);
+            anchorScroll();
+        }
         ContactUs.find({}, function(response){
             if(response && response.result){
                 response = response.result;
             }
             scope.contaceDataArr = response;
-            console.log("get contact data response: ", response);
         }, function(err){
             console.log("get contact data err: ", err);
         })
--- client/js/main.js
+++ client/js/main.js
@@ -2,16 +2,18 @@
         //Controller
 (function (angular) {
  
-            app.controller('mainController', ['$scope', '$location','$state', 'Contact_us', function (scope, location,state, ContactUs) {
+            app.controller('mainController', ['$scope', '$location','$state', 'Contact_us', '$anchorScroll', function (scope, location,state, ContactUs, anchorScroll) {
+                scope.scrollTo = function(id) {
+                    location.hash(id);
+                    anchorScroll();
+                }
                 scope.submitContactUsForm = function(){
                 	var contactName = scope.contactName;
                 	var contactEmail = scope.contactEmail;
                 	var contactSubject = scope.contactSubject;
                 	var contactMessage = scope.contactMessage;
                 	var newContactObj = {name:contactName, email: contactEmail, subject: contactSubject, message: contactMessage};
-                    console.log("newContactObj: ", newContactObj);
                     ContactUs.create(newContactObj, function(response){
-                        console.log("contact data created: ", response);
                         state.go('app.dashboard');
                     }, function(err){
                         console.log("contact data err: ", err);
